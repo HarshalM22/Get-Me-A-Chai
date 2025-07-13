b078e62f5a03159c615d4adc72468f5e0a6e94a3
HarshalM22 <harshalmankar61@gmail.com>
2025-07-13 15:19:53 +0530

    login page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function Navbar() {
   return (
@@ -5,12 +6,12 @@ export default function Navbar() {
        
        <div className="logo font-bold text-xl">GetMEAChai!</div>
        
-        <ul className="flex justify-between gap-4">
+        <ul className="flex justify-between items-center gap-4">
             <li>Home</li>
             <li>About</li>
             <li>Projects</li>
             <li>Sign Up</li>
-            <li>Login </li>
+           <Link href={"/login"}> <button  className="cursor-pointer text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2 text-center ">Login</button></Link>
         </ul>
         
       
